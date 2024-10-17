4d15399396b0b422463d6b3e1b9f2c1e5d1e75f9
Abass Olaiya <ayotundeolaiya@gmail.com>
2024-10-17 09:38:04 +0100

    Add files via upload

---
+++ precache-manifest.e783aef1f0c4e2a6b4a14970bff080b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999efb262ae0b41d3292b1d3fcd08566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03003fcc9307db902f3b",
+    "url": "/static/css/2.ee310471.chunk.css"
+  },
+  {
+    "revision": "c054665d89d6fbe22324",
+    "url": "/static/css/main.7f659cd6.chunk.css"
+  },
+  {
+    "revision": "03003fcc9307db902f3b",
+    "url": "/static/js/2.2a9412d5.chunk.js"
+  },
+  {
+    "revision": "b67884c3299fdd5681e916d89d4271fd",
+    "url": "/static/js/2.2a9412d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c054665d89d6fbe22324",
+    "url": "/static/js/main.588e5453.chunk.js"
+  },
+  {
+    "revision": "c388439a47295744d5aa",
+    "url": "/static/js/runtime~main.135f15be.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
